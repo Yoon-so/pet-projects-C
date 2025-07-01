2f8d7b9c0bdccc7f9ca50984917fc2996fb481b3
Ekaterina <yoonrin113@gmail.com>
2025-07-01 16:11:18 +0900

    Add: English and Korean versions of project1

---
+++ project1/english/main.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int id, pass; //Declare variables to store the ID and password
+	printf("Please enter your ID and password using 4 digits.\n");
+
+	printf("id: ____\b\b\b\b"); //Code to remove underline when entering the ID
+	scanf("%d", &id); //Receive ID input
+
+	printf("pass: ____\b\b\b\b"); //Code to remove underline when entering the Password
+	scanf("%d", &pass); //Receive Password input
+
+	//Print received ID and Password
+	printf("The ID entered is \"%d\" and the password is \"%d\"\n", id, pass);
+	return 0;
+}
