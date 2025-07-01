019d4917f7e350376eb47713336be07100694f83
Ekaterina <yoonrin113@gmail.com>
2025-07-01 16:56:24 +0900

    Add: new versions of main_kr.c and main_en.c

---
+++ 1.Calculate_the_sum/main_en.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int x, y, z, sum;
+	sum = 0; //Initialize variable to store the total sum
+
+	printf("Enter 3 integers (x, y, z): "); //Print a message to enter 3 integers
+	scanf("%d %d %d", &x, &y, &z); //Receive three integers as input
+
+	sum += x; //Add the value of x to the total sum
+	sum += y; //Add the value of y to the total sum
+	sum += z; //Add the value of z to the total sum
+
+	printf("Sum of three integers: %d\n", sum); //Print the sum of the 3 integers
+	return 0;
+}
---
+++ 1.Calculate_the_sum/main_kr.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int x, y, z, sum;
+	sum = 0; //합계를 저장할 변수 초기화
+
+	printf("Enter 3 integers (x, y, z): "); +//3개의 정수를 입력하라는 메시지 출력 
+	scanf("%d %d %d", &x, &y, &z); //3개의 정수를 입력받기
+
+	sum += x; //x값을 합계에 더하기
+	sum += y; //y값을 합계에 더하기
+	sum += z; //z값을 합계에 더하기
+
+	printf("Sum of three integers: %d\n", sum); //3개의 정수의 합계를 출력
+	return 0;
+}
