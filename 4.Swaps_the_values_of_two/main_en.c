9032bf3043bb5029f133177faf2a78f4fd2756bd
Ekaterina <yoonrin113@gmail.com>
2025-07-04 23:45:57 +0900

    Add: main_en.c (English version)

---
+++ 4.Swaps_the_values_of_two/main_en.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int x = 10; //Initialize x value
+	int y = 20; //Initialize y value
+	printf("Before: x=%d y=%d\n", x, y); //Print initial values of x and y
+
+	int tmp; //Declare a temporary variable for swapping values
+	tmp = x; //Store the value of x in tmp
+	x = y;	 //Assign the value of y to x
+	y = tmp; //Assign the value stored in tmp (original x) to y
+
+	printf("After: x=%d y=%d\n", x, y); //Print values of x and y after swapping
+
+	return 0;
+}
