6b8cfc1ad4d02a68be1cd5a50026ae9830e15784
Ekaterina <yoonrin113@gmail.com>
2025-07-04 23:11:11 +0900

    Add: main_en.c (English version)

---
+++ 3.Sunlight_Travel_Time/main_en.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	double light_speed = 300000; //Initialize speed of light (unit: km/s)
+	double distance = 149600000; //Initialize distance between the Sun and the Earth (unit: km)
+
+	double time; //Declare time variable
+
+	time = distance / light_speed; //Calculate time: distance / speed
+
+	printf("Speed: %f km/s\n", light_speed); //Print speed of light
+	printf("Distance: %f km\n", distance); //Print distance
+	printf("Time: %f seconds\n", time); //Print calculated time
+
+	return 0;
+}
