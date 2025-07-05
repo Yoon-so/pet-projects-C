ae93b96b6ee7acee851abc50510e6d5433140386
Ekaterina <yoonrin113@gmail.com>
2025-07-05 23:10:49 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 5.Calculate_the_volume_of_a_box/main_en.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	double width, length, height, volume; //Declare variables to store width, length, height, and volume of the box
+
+	//Display a message prompting the user to enter width, length, and height at once
+	printf("Enter the width, length and height of the box at once: ");
+	scanf("%lf %lf %lf", &width, &length, &height); //Read the input values
+
+	volume = width * length * height; //Calculate volume using the formula: volume = width * length * height
+	printf("The volume of the box is %lf\n", volume); //Display the calculated volume
+
+	return 0;
+}
---
+++ 5.Calculate_the_volume_of_a_box/main_kr.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	double width, length, height, volume; //상자의 가로, 세로, 높이, 부피를 저장할 변수 선언
+
+	//상자의 가로, 세로, 높이를 한번에 입력 받기 위한 메시지 출력
+	printf("Enter the width, length and height of the box at once: ");
+	scanf("%lf %lf %lf", &width, &length, &height); //입력 받음
+
+	volume = width * length * height; //입력된 가로, 세로, 높이를 사용하여 부피를 계산 (부피 = 깊이 * 너비 * 높이)
+	printf("The volume of the box is %lf\n", volume); //계산된 부피를 출력
+
+	return 0;
+}
