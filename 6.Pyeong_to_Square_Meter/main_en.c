d033d197d7f43ef89f7044a4fbe417421a67c19c
Ekaterina <yoonrin113@gmail.com>
2025-07-06 01:40:33 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 6.Pyeong_to_Square_Meter/main_en.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	const double SQMETER_PER_PYEONG = 3.3058; //Define a constant to convert 1 pyeong to square meters
+	double pyeong, result; //Variables to store the input in pyeong and the conversion result
+
+	printf("Please enter pyeong: "); //Display a message to prompt the user to enter pyeong
+	scanf("%lf", &pyeong); // Store the entered pyeong value in the variable pyeong
+
+	result = pyeong * SQMETER_PER_PYEONG; //Convert the entered pyeong value to square meters
+	printf("It's %lf square meters\n", result); //Output the result in square meters
+
+	return 0;
+}
---
+++ 6.Pyeong_to_Square_Meter/main_kr.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	const double SQMETER_PER_PYEONG = 3.3058; //1평을 제곱미터로 변환하기 위한 상수 정의
+	double pyeong, result; //평수를 입력받을 변수와 결과를 저장할 변수
+
+	printf("Please enter pyeong: "); //평수를 입력하라는 메시지 출력
+	scanf("%lf", &pyeong); //입력된 평수를 변수 pyeong에 저장
+
+	result = pyeong * SQMETER_PER_PYEONG; //입력된 평수를 제곱미터로 변환
+	printf("It's %lf square meters\n", result); //변환된 제곱미터 결과를 출력
+
+	return 0;
+}
