161dd4013e1d124102172ac26f7591768d6e39c1
Ekaterina <yoonrin113@gmail.com>
2025-07-13 16:28:13 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 8.ASCII_Character_Incrementation/main_en.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	// Print the characters resulting from adding 1, 2, and 3 to the ASCII code of 'a'
+	// The ASCII code of 'a' is 97,
+	// 'a' + 1 = 'b' (ASCII code 98),
+	// 'a' + 2 = 'c' (ASCII code 99),
+	// 'a' + 3 = 'd' (ASCII code 100)
+	printf("%c = %d\n", 'a', 'a');
+	printf("%c = %d\n", 'a' + 1, 'a' + 1);
+	printf("%c = %d\n", 'a' + 2, 'a' + 2);
+	printf("%c = %d\n", 'a' + 3, 'a' + 3);
+
+	return 0;
+}
---
+++ 8.ASCII_Character_Incrementation/main_kr.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	// 'a'의 ASCII 코드 값에 1, 2, 3을 더한 결과를 문자로 출력
+	// 'a'의 ASCII 코드 값은 97,
+	// 'a' + 1 = 'b' (ASCII 코드 98),
+	// 'a' + 2 = 'c' (ASCII 코드 99),
+	// 'a' + 3 = 'd' (ASCII 코드 100)
+	printf("%c = %d\n", 'a', 'a');
+	printf("%c = %d\n", 'a' + 1, 'a' + 1);
+	printf("%c = %d\n", 'a' + 2, 'a' + 2);
+	printf("%c = %d\n", 'a' + 3, 'a' + 3);
+
+	return 0;
+}
