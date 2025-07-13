4d2edb4dac05b10633b9fffd61cc361ba8e4be88
Ekaterina <yoonrin113@gmail.com>
2025-07-13 15:14:26 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 7.Kinetic_Energy_Calculator/main_en.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	double mass, speed, kenergy; //Declare variables to store mass(m), speed(v) and kinetic energy(e)
+	
+	printf("Mass(kg): "); //Print a message to enter mass
+	scanf("%lf", &mass); //Store the entered mass in the variable 'mass'
+
+	printf("Speed(m/s): "); //Print a message to enter speed
+	scanf("%lf", &speed); //Store the entered speed in the variable 'speed'
+
+	kenergy = (1.0 / 2.0) * mass * speed * speed; //Calculate kinetic energy (Formula: E = 1/2 * m * v * v)
+	printf("Kinetic energy(J): %lf\n", kenergy); //Print the calculated kinetic energy
+
+	return 0;
+}
---
+++ 7.Kinetic_Energy_Calculator/main_kr.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	double mass, speed, kenergy; //질량(m), 속도(v), 운동에너지(e)를 저장할 변수 선언
+	
+	printf("Mass(kg): "); //질량을 입력 받기 위한 메시지 출력
+	scanf("%lf", &mass); //입력한 질량을 변수 mass에 저장
+
+	printf("Speed(m/s): "); //속도를 입력 받기 위한 메시지 출력
+	scanf("%lf", &speed); //입력한 속도를 변수 speed에 저장
+
+	kenergy = (1.0 / 2.0) * mass * speed * speed; //운동에너지 계산 (공식: E = 1/2 * m * v * v)
+	printf("Kinetic energy(J): %lf\n", kenergy); //계산된 운동에너지를 출력
+
+	return 0;
+}
