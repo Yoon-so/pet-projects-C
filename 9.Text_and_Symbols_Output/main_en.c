938ff0475f8610ddf75506f050cf56fc8f8406b1
Ekaterina <yoonrin113@gmail.com>
2025-07-13 16:31:23 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 9.Text_and_Symbols_Output/main_en.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	printf("\"ASCII code\", 'A', 'B', 'C'\n"); //Print the string: "ASCII code", 'A', 'B', 'C'
+	printf("\\t \\a \\n\n"); //Print the string: \t \a \n
+
+	return 0;
+}
---
+++ 9.Text_and_Symbols_Output/main_kr.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	printf("\"ASCII code\", 'A', 'B', 'C'\n"); //"ASCII code", 'A', 'B', 'C' 문자열을 출력
+	printf("\\t \\a \\n\n"); // \t \a \n 문자열을 출력
+
+	return 0;
+}
