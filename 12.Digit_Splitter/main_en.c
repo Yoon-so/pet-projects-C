949a9780eca045cdd12eca510e777c05483e1a40
Ekaterina <yoonrin113@gmail.com>
2025-07-27 23:51:20 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 12.Digit_Splitter/main_en.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int value; //Declare a variable to store the input integer
+
+	printf("Enter an integer: ");
+	scanf("%d", &value); //Store the entered integer into the variable 'value'
+
+	printf("Tens digit: %d\n", value / 10); //Divide value by 10 and get the tens digit
+	printf("Ones digit: %d\n", value % 10); //Get the ones digit by taking modulo 10
+
+	return 0;
+}
---
+++ 12.Digit_Splitter/main_kr.c
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int value; //입력받을 정수를 저장할 변수 선언
+
+	printf("Enter an integer: "); //'정수를 입력하시오:' 메시지 출력
+	scanf("%d", &value); //입력된 정수를 변수 value에 저장 
+
+	printf("Tens digit: %d\n", value / 10); //value를 10으로 나누어 십의 자리 출력 
+	printf("Ones digit: %d\n", value % 10); //value를 10으로 나눈 일의 자리 출력
+
+	return 0;
+}
