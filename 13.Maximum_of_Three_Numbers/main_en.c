651eced604311b13e0e9ee864ff361e7147724f3
Ekaterina <yoonrin113@gmail.com>
2025-08-07 23:58:01 +0900

    Add: main_kr.c (Korean version) and main_en.c (English version)

---
+++ 13.Maximum_of_Three_Numbers/main_en.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int x, y, z, max; //Declare three numbers and variable for the maximum
+
+	printf("Enter 3 integers: "); //Prompt to enter 3 integers
+	scanf("%d %d %d", &x, &y, &z); //Store enteres values in x, y, z
+
+	max = (x > y) ? x : y; //Store the larger of x and y in max
+	max = (max > z) ? max : z; //Compare max with z and store the larger value
+
+	printf("Maximum value: %d\n", max); //Print the maximum value
+
+	return 0;
+}
---
+++ 13.Maximum_of_Three_Numbers/main_kr.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int x, y, z, max; //정수 3개와 최대값 변수 선언
+
+	printf("Enter 3 integers: "); //"정수 3개를 입력하시오:" 출력
+	scanf("%d %d %d", &x, &y, &z); //입력된 정수를 x, y, z에 저장
+
+	max = (x > y) ? x : y; //x와 y 중에서 더 큰 값을 max에 저장
+	max = (max > z) ? max : z; //max와 z 중에서 더 큰 값을 다시 max에 저장
+
+	printf("Maximum value: %d\n", max); //최대값을 출력
+
+	return 0;
+}
